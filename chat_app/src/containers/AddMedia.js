11c4a2c211e36b71897dd805b900fe11c9ce7855
CreateDesign.tech <info@createdesign.tech>
2018-08-20 22:35:03 -0700

    prepare for cropper-picker integration

--- chat_app/src/containers/AddMedia.js
+++ chat_app/src/containers/AddMedia.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import Icon from 'react-native-vector-icons/FontAwesome'
@@ -8,11 +8,20 @@ import { ActionCreators } from '../actions/index'
 class AddMedia extends Component<{}> {
     render() {
         return (
-            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-                <Icon name="camera" style={{alignSelf: 'center', fontSize: 60}}/>
-                <Text style={{textAlign: 'center', fontSize: 30}}>
-                    Add Media
-                </Text>
+            <View style={styles.sections}>
+                <TouchableOpacity style={styles.buttons}>
+                    <Icon name="camera" style={{alignSelf: 'center', fontSize: 60}}/>
+                    <Text style={{textAlign: 'center', fontSize: 30}}>
+                        Camera
+                    </Text>
+                </TouchableOpacity>
+                <View style={styles.divider}/>
+                <TouchableOpacity style={styles.buttons}>
+                    <Icon name="image" style={{alignSelf: 'center', fontSize: 60}}/>
+                    <Text style={{textAlign: 'center', fontSize: 30}}>
+                        Camera
+                    </Text>
+                </TouchableOpacity>
             </View>
         )
     }
@@ -29,3 +38,23 @@ function mapDispatchToProps(dispatch){
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(AddMedia)
+
+const styles = StyleSheet.create({
+    sections: {
+        width: '100%',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    buttons: {
+        flex: 1,
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    divider: {
+        width: '90%',
+        height: 2,
+        backgroundColor: '#eee'
+    }
+})
