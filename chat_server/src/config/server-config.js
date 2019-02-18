965dea9b347b4bfca104ac7d1f5d2567301b48e4
Aaron Shier <info@createdesign.tech>
2019-02-17 19:53:53 -0800

    Update server-config.js

--- chat_server/src/config/server-config.js
+++ chat_server/src/config/server-config.js
@@ -1,20 +1,20 @@
 import { production } from './index'
 
-export const server = production ? `https://ohshutit.app` : `http://localhost:8080`
+export const server = production ? `https://yourdomain.com` : `http://localhost:8080`
 
 import { secret as jwtSecret } from './index'
 
 export const secret = jwtSecret
 
 export const googleAuth = {
-  clientID: '443541868618-98u41tbu7r8h2j25d8m87em6au3af9tp.apps.googleusercontent.com',
-  clientSecret: 'zfgooO9bgy9l9_k0tYZPaP3N',
+  clientID: 'XXX',
+  clientSecret: 'XXX',
   callbackURL: `${server}/auth/google/callback`,
 }
 
 export const facebookAuth = {
-  clientID: '1347027622099102',
-  clientSecret: 'b35d42a92dbd925b4edbe7d5b4648fcb',
+  clientID: 'XXX',
+  clientSecret: 'XXX',
   callbackURL: `${server}/auth/facebook/callback`,
 }
 
