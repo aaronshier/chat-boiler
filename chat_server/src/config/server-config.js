86b2d58702496bffcf1e7f3b21c323e207698100
Aaron Shier <info@createdesign.tech>
2019-02-17 19:52:53 -0800

    Update server-config.js

--- chat_server/src/config/server-config.js
+++ chat_server/src/config/server-config.js
@@ -7,14 +7,14 @@ import { secret as jwtSecret } from './index'
 export const secret = jwtSecret
 
 export const googleAuth = {
-  clientID: 'YOUR_CLIENT_KEY',
-  clientSecret: 'YOUR_SECRET_KEY',
+  clientID: '443541868618-98u41tbu7r8h2j25d8m87em6au3af9tp.apps.googleusercontent.com',
+  clientSecret: 'zfgooO9bgy9l9_k0tYZPaP3N',
   callbackURL: `${server}/auth/google/callback`,
 }
 
 export const facebookAuth = {
-  clientID: 'YOUR_CLIENT_ID',
-  clientSecret: 'YOUR_SECRET_KEY',
+  clientID: '1347027622099102',
+  clientSecret: 'b35d42a92dbd925b4edbe7d5b4648fcb',
   callbackURL: `${server}/auth/facebook/callback`,
 }
 
